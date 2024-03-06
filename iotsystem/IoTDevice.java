152f74aa7d1d4c84b1fa9f0a7f69d5995da95b4a
Bruna Santos <brunafrsantos@gmail.com>
2024-03-06 19:59:37 +0000

    Started Connection & Authentication methods

--- iotsystem/IoTDevice.java
+++ iotsystem/IoTDevice.java
@@ -1,5 +1,6 @@
 package iotsystem;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -35,7 +36,16 @@ public class IoTDevice {
         password = sc.nextLine();
 
         // Connection & Authentication
-        // TODO
+        connectDevice(serverAdress);
+        deviceAuth(userid, password);
+        printMenu();
+        // Enquanto o utilizador não pressionar CTRL+C, o programa deverá voltar ao
+        // passo 9. Caso contrário, termina.
+        while (true) {
+            System.out.print("> ");
+            String command = sc.nextLine();
+            // TODO Execute comand
+        }
     }
 
     /**
@@ -53,6 +63,76 @@ public class IoTDevice {
                 + " do domínio <dm>, desde que o utilizador tenha permissões.");
         System.out.println("RI <user-id>:<dev_id> # Receber o ficheiro Imagem do dispositivo "
                 + "<user-id>:<dev_id> do servidor, desde que o utilizador tenha permissões.");
+
+    }
+
+    /**
+     * Takes a server adress String and connects the client to the specified server.
+     * 
+     * @param serverAddress - String {@code <serverAddress>} that identifies the
+     *                      server. Format: {@code<IP/hostname>[:Port]}
+     */
+    private static void connectDevice(String serverAddress) {
+        // Check if port was given
+        String[] address_port = serverAddress.split(":");
+        String addr = new String(address_port[0]);
+        int port = address_port.length > 1 ? Integer.parseInt(address_port[1]) : DEFAULT_PORT;
+
+        // Try server connection
+        try {
+            System.out.println("Connecting to server.");
+            clientSocket = new Socket(addr, port);
+            System.out.println("Connection successful - " + addr + ":" + port);
+        } catch (IOException e) {
+            System.err.println("ERROR" + e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * TODO
+     * 
+     * @param user
+     * @param password
+     * @param serverAdress
+     */
+    private static void deviceAuth(String user, String password) {
+
+        try {
+            System.out.println("Starting authentication.");
+            in = new ObjectInputStream(clientSocket.getInputStream());
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            boolean auth = false;
+
+            do {
+                MessageCode code = (MessageCode) in.readObject();
+                switch (code) {
+                    case WRONG_PWD:
+                        System.out.println(MessageCode.WRONG_PWD.getDesc());
+                        // TODO
+                        break;
+                    case OK_NEW_USER:
+                        System.out.println(MessageCode.OK_NEW_USER.getDesc());
+                        // TODO
+                        break;
+                    case OK_USER:
+                        System.out.println(MessageCode.OK_USER.getDesc());
+                        // TODO
+                        auth = true;
+                        break;
+                    default:
+                        break;
+                }
+            } while (!auth);
+
+        } catch (IOException e) {
+            System.err.println("ERROR" + e.getMessage());
+            System.exit(-1);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(-1);
+        }
 
     }
 }
