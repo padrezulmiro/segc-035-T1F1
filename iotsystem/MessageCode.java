50523611e01bc133956b4ab1cfaa3da619f5dc47
Bruna Santos <brunafrsantos@gmail.com>
2024-03-06 19:23:16 +0000

    Created enum to represent messages/error codes

---
+++ iotsystem/MessageCode.java
@@ -0,0 +1,30 @@
+package iotsystem;
+
+/**
+ * Represents messages to be sent between server and client.
+ */
+public enum MessageCode {
+    WRONG_PWD("WRONG_PWD # Wrong password. Try again."),
+    OK_NEW_USER("OK_NEW_USER # This user isn't registered."),
+    OK_USER("OK_USER # User exists and pswd is correct."),
+    NOK_DEVID("NOK_DEVID # This device ID is already connected with this user. Try another <dev-id>."),
+    OK_DEVID("OK_DEVID # User and device."),
+    NOK_TESTED("NOK_TESTED # Server did not validate."),
+    OK_TESTED("OK_TESTED # Server validated."),
+    OK("OK"),
+    NODM("NODM # Domain does not exist."),
+    NOPERM("NOPERM # User doesn't have read permission."),
+    NODATA("NODATA # Device id has no published data."),
+    NOID("NOID # Device id does not exist.");
+
+    private String desc;
+
+    MessageCode(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+}
