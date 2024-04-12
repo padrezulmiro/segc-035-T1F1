728325f24a5350e6b47018d580241bd1267ecfa2
aZul <fmdcosta.piano@gmail.com>
2024-04-12 17:12:19 +0100

    refactor: Extract code from looping switch

--- iotserver/ServerThread.java
+++ iotserver/ServerThread.java
@@ -24,95 +24,49 @@ public class ServerThread extends Thread {
         this.isRunning = true;
     }
 
-    public void run() { 
+    public void run() {
         System.out.println("Accepted connection!");
 
         try {
-
             out = new ObjectOutputStream(socket.getOutputStream());
-            in = new ObjectInputStream(socket.getInputStream()); //socket is closed here
+            in = new ObjectInputStream(socket.getInputStream());
             manager = ServerManager.getInstance();
-            MessageCode res;
 
             while(isRunning){
                 MessageCode opcode = (MessageCode) in.readObject();
                 switch (opcode) {
                     case AU:
-                        if (this.userID==null){
-                            this.userID = (String)in.readObject();
-                        }
-                        String pwd = (String)in.readObject();
-                        out.writeObject(manager.authenticateUser(userID,pwd).responseCode());
+                        authUser();
                         break;
                     case AD:
-                        String deviceID = (String)in.readObject();
-                        res = manager.authenticateDevice(userID,deviceID).responseCode();
-                        if(res == MessageCode.OK_DEVID){this.deviceID = deviceID;}
-                        out.writeObject(res);
+                        authDevice();
                         break;
                     case TD:
-                        String fileName = (String)in.readObject();
-                        long fileSize = (long)in.readLong();
-                        res = manager.testDevice(fileName, fileSize).responseCode();
-                        out.writeObject(res);
-                        // System.out.println("Correct " + res + "filename=" + fileName + " size=" + fileSize);
+                        testDevice();
                         break;
                     case CREATE:
-                        String domain = (String)in.readObject();
-                        res = manager.createDomain(userID,domain).responseCode();
-                        out.writeObject(res);
+                        createDomain();
                         break;
                     case ADD:
-                        String newUser = (String)in.readObject();
-                        domain = (String)in.readObject();
-                        res = manager.addUserToDomain(userID, newUser, domain).responseCode();
-                        out.writeObject(res);
+                        addUserToDomain();
                         break;
                     case RD:
-                        domain = (String)in.readObject();
-                        res = manager.registerDeviceInDomain(domain, this.userID, this.deviceID).responseCode();
-                        out.writeObject(res);
+                        registerDeviceInDomain();
                         break;
                     case ET:
-                        String tempStr = (String)in.readObject();
-                        res = manager.registerTemperature(tempStr,this.userID,this.deviceID).responseCode();
-                        out.writeObject(res);
-                        out.flush();
+                        registerTemperature();
                         break;
                     case EI:
-                        // input image stream
-                        String filename = (String)in.readObject();
-                        fileSize = (long)in.readObject();
-                        res = manager.registerImage(filename,fileSize,in,this.userID,this.deviceID).responseCode(); 
-                        out.writeObject(res);
+                        registerImage();
                         break;
                     case RT:
-                        domain = (String)in.readObject();
-                        ServerResponse sResponse = manager.getTemperatures(this.userID,domain);
-                        //getfilestream write it?
-                        res = sResponse.responseCode();
-                        out.writeObject(res);
-                        if(res==MessageCode.OK){
-                            // FileHelper.sendFile(sResponse.filePath(),out);
-                            out.writeObject(sResponse.temperatures());
-                        }
+                        getTemperatures();
                         break;
                     case RI:
-                        String targetUser = (String)in.readObject();
-                        String targetDev = (String)in.readObject();
-                        ServerResponse sr = manager.getImage(this.userID,targetUser, targetDev);
-                        MessageCode rCode=sr.responseCode();
-                        // Send code to client
-                        out.writeObject(rCode);
-                        // Send file (if aplicable)
-                        if( rCode == MessageCode.OK){
-                            FileHelper.sendFile(sr.filePath(), out);
-                        }
+                        getImage();
                         break;
                     case STOP:
-                        System.out.println("Client quits. killing thread");
-                        manager.disconnectDevice(this.userID, this.deviceID);
-                        isRunning = false;
+                        stopThread();
                         break;
                 }
             }
@@ -122,5 +76,91 @@ public class ServerThread extends Thread {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    private void stopThread() {
+        System.out.println("Client quits. killing thread");
+        manager.disconnectDevice(this.userID, this.deviceID);
+        isRunning = false;
+    }
+
+    private void getImage() throws IOException, ClassNotFoundException {
+        String targetUser = (String)in.readObject();
+        String targetDev = (String)in.readObject();
+        ServerResponse sr = manager.getImage(this.userID,targetUser, targetDev);
+        MessageCode rCode=sr.responseCode();
+        // Send code to client
+        out.writeObject(rCode);
+        // Send file (if aplicable)
+        if( rCode == MessageCode.OK){
+            FileHelper.sendFile(sr.filePath(), out);
+        }
+    }
+
+    private void getTemperatures() throws IOException, ClassNotFoundException {
+        String domain = (String) in.readObject();
+        ServerResponse sResponse = manager.getTemperatures(this.userID,domain);
+        MessageCode res = sResponse.responseCode();
+        out.writeObject(res);
+        if(res==MessageCode.OK){
+            // FileHelper.sendFile(sResponse.filePath(),out);
+            out.writeObject(sResponse.temperatures());
+        }
+    }
+
+    private void registerImage() throws IOException, ClassNotFoundException {
+        String filename = (String)in.readObject();
+        long fileSize = (long)in.readObject();
+        MessageCode res = manager.registerImage(filename,fileSize,in,this.userID,this.deviceID).responseCode();
+        out.writeObject(res);
+    }
+
+    private void registerTemperature() throws IOException, ClassNotFoundException {
+        String tempStr = (String)in.readObject();
+        MessageCode res = manager.registerTemperature(tempStr,this.userID,this.deviceID).responseCode();
+        out.writeObject(res);
+        out.flush();
+    }
+
+    private void registerDeviceInDomain() throws IOException, ClassNotFoundException {
+        String domain = (String)in.readObject();
+        MessageCode res = manager.registerDeviceInDomain(domain, this.userID, this.deviceID).responseCode();
+        out.writeObject(res);
+    }
+
+    private void addUserToDomain() throws IOException, ClassNotFoundException {
+        String newUser = (String)in.readObject();
+        String domain = (String)in.readObject();
+        MessageCode res = manager.addUserToDomain(userID, newUser, domain).responseCode();
+        out.writeObject(res);
+    }
+
+    private void createDomain() throws IOException, ClassNotFoundException {
+        String domain = (String)in.readObject();
+        MessageCode res = manager.createDomain(userID,domain).responseCode();
+        out.writeObject(res);
+    }
+
+    private void testDevice() throws IOException, ClassNotFoundException {
+        String fileName = (String)in.readObject();
+        long fileSize = (long)in.readLong();
+        MessageCode res = manager.testDevice(fileName, fileSize).responseCode();
+        out.writeObject(res);
+        // System.out.println("Correct " + res + "filename=" + fileName + " size=" + fileSize);
+    }
+
+    private void authDevice() throws IOException, ClassNotFoundException {
+        String deviceID = (String)in.readObject();
+        MessageCode res = manager.authenticateDevice(userID,deviceID).responseCode();
+        if(res == MessageCode.OK_DEVID){this.deviceID = deviceID;}
+        out.writeObject(res);
+    }
+
+    private void authUser() throws IOException, ClassNotFoundException {
+        if (this.userID==null){
+            this.userID = (String) in.readObject();
+        }
+        String pwd = (String) in.readObject();
+        out.writeObject(manager.authenticateUser(userID,pwd).responseCode());
     }
 }
