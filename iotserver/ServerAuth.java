d08ed09a05c5fae2b4e2a20648d6458d0de67fe3
Bruna Santos <fc56328@alunos.ciencias.ulisboa.pt>
2024-04-24 15:35:39 +0100

    Squashed commit of the following:
    commit 674cfcb66e70b80dedf1418264ebbcca0de318f2
    Merge: 29781df 5febf7d
    Author: Mimi <90327325+enderesting@users.noreply.github.com>
    Date:   Tue Apr 23 22:24:11 2024 +0100
    
        Merge pull request #39 from padrezulmiro/client-2FA
    
        2FA - Add missing imports
    
    commit 5febf7d324b35641035f8c2356e292c3a4eef12d
    Author: Bruna Santos <brunafrsantos@gmail.com>
    Date:   Tue Apr 23 22:20:46 2024 +0100
    
        Add missing import
    
    commit 29781df7456c8bb1c1bb917197668abd9c1cb034
    Merge: 131f287 faa41ff
    Author: Mimi <90327325+enderesting@users.noreply.github.com>
    Date:   Tue Apr 23 22:19:11 2024 +0100
    
        Merge pull request #37 from padrezulmiro/client-2FA
    
        Implement two-factor authentication
    
    commit faa41ffa75f8096724d9a5a6c2554f92d68f7f32
    Author: Bruna Santos <brunafrsantos@gmail.com>
    Date:   Tue Apr 23 22:18:01 2024 +0100
    
        Move Utils.java
    
    commit 1220962cb93b512fcc5e3ee22bf8738e62361265
    Author: Bruna Santos <fc56328@alunos.ciencias.ulisboa.pt>
    Date:   Tue Apr 23 17:46:59 2024 +0100
    
        Fix typo
    
    commit 87a425079902595a119613d22b17d71bcd5ff71e
    Author: Bruna Santos <fc56328@alunos.ciencias.ulisboa.pt>
    Date:   Tue Apr 23 17:44:54 2024 +0100
    
        Implement certificate storing
    
    commit 33dae2fc4b9e4448cf3bb8c86bd709d05a87d8a5
    Author: Bruna Santos <fc56328@alunos.ciencias.ulisboa.pt>
    Date:   Tue Apr 23 17:37:45 2024 +0100
    
        Change scerts folder path

--- iotserver/ServerAuth.java
+++ iotserver/ServerAuth.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.ByteBuffer;
